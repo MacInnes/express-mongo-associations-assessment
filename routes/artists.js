8f8ed197f91e28ea0c2b1f0ff6528e68c3d8f01e
Andrew Mockett <califormula@gmail.com>
2015-11-03 03:54:10 -0700

    4 am commit, time for bed

--- routes/artists.js
+++ routes/artists.js
@@ -34,49 +34,8 @@ router.post('/edit/:id', function(req, res, next){
 router.post('/', function(req, res, next) {
   var errors = [];
   //lib file db function to insert an artist goes here
-  if (req.body.artistSelectBox === 'none' || req.body.artistSelectBox === undefined){
-    //validations
-    dbcalls.findArtist(req.body.artist).then(function(data){
-      if(data){
-        errors.push('Artist already exists');
-        dbcalls.findArtists().then(function(data){
-          res.render('artists', {title: 'ARTISTS', artists: data, errors: errors})
-        })
-        
-      } else {
-        dbcalls.insertArtist(req.body.artist).then(function(data){
-          var artistID = data._id;
-          dbcalls.findAlbum(req.body.album).then(function(data){
-            if(data){
-              errors.push('Album already exists, please edit it to make changes.');
-              dbcalls.findArtists().then(function(data){
-                res.render('artists', {title: 'ARTISTS', artists: data, errors: errors})
-              })
-            } else {
-              dbcalls.insertAlbum(req.body.album, req.body.artist).then(function(data){
-                console.log('Album inserted')
-                dbcalls.findArtists().then(function(data){
-                  res.render('artists', {title: 'ARTISTS', artists: data, errors: errors})
-                })
-              })
-            }
-          })
-        })
-      }
-    });
-    
-  } else {
-    //validations
-    dbcalls.findArtist(req.body.artistSelectBox).then(function(data){
-      if(data){
-        console.log('artist already exists')
-      }
-    }, function(reason){
-      dbcalls.insertArtist(req.body.artistSelectBox).then(function(data){
-        console.log('no artist found')
-        res.render('index');
-      })
-    })
-  }
+  dbcalls.insertArtist(req.body.artist).then(function(data){
+    res.redirect('/artists');
+  })
 })
 module.exports = router;
